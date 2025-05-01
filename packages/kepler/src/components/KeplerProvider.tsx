ff0e5ba8f8dd28d03d446d0849a2a236d3953db0
Shan He <heshan0131@gmail.com>
2025-05-01 15:52:16 -0700

    fix layer order
    Signed-off-by: Shan He <heshan0131@gmail.com>

--- packages/kepler/src/components/KeplerProvider.tsx
+++ packages/kepler/src/components/KeplerProvider.tsx
@@ -1,10 +1,10 @@
 import {messages} from '@kepler.gl/localization';
 import {useMemo} from 'react';
 import {IntlProvider} from 'react-intl';
-import {ThemeProvider} from 'styled-components';
+import {ThemeProvider, StyleSheetManager} from 'styled-components';
 import {KeplerGlReduxState, useStoreWithKepler} from '../KeplerSlice';
 
-import {KeplerGlContext} from '@kepler.gl/components';
+import {KeplerGlContext, shouldForwardProp} from '@kepler.gl/components';
 import {theme} from '@kepler.gl/styles';
 import {Provider} from 'react-redux';
 
@@ -32,11 +32,13 @@ export const KeplerProvider: React.FC<KeplerProviderProps> = ({
   return (
     <IntlProvider locale="en" messages={messages['en']}>
       <Provider store={reduxProviderStore}>
-        <ThemeProvider theme={theme}>
-          <KeplerGlContext.Provider value={keplerContext}>
-            {children}
-          </KeplerGlContext.Provider>
-        </ThemeProvider>
+        <StyleSheetManager shouldForwardProp={shouldForwardProp}>
+          <ThemeProvider theme={theme}>
+            <KeplerGlContext.Provider value={keplerContext}>
+              {children}
+            </KeplerGlContext.Provider>
+          </ThemeProvider>
+        </StyleSheetManager>
       </Provider>
     </IntlProvider>
   );
