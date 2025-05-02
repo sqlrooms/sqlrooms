d2bfb278a99f52dc0c587ad21594021f4e8c2c14
Shan He <heshan0131@gmail.com>
2025-05-02 01:03:12 -0700

    import bottomwidget
    Signed-off-by: Shan He <heshan0131@gmail.com>

--- packages/kepler/src/components/KeplerMapContainer.tsx
+++ packages/kepler/src/components/KeplerMapContainer.tsx
@@ -2,25 +2,30 @@ import {FC, useRef} from 'react';
 
 import {
   MapContainerFactory,
+  BottomWidgetFactory,
   mapFieldsSelector,
+  bottomWidgetSelector,
   MapViewStateContextProvider,
-  RootContext
+  RootContext,
 } from '@kepler.gl/components';
+import {useTheme} from 'styled-components';
+
 import {KeplerInjector} from './KeplerInjector';
 import {KeplerProvider} from './KeplerProvider';
 import {useKeplerStateActions} from '../hooks/useKeplerStateActions';
+// import {theme} from '@kepler.gl/styles';
 
 const MapContainer = KeplerInjector.get(MapContainerFactory);
+const BottomWidget = KeplerInjector.get(BottomWidgetFactory);
 
-// @ts-expect-error: injected with define
 const MAPBOX_TOKEN = process.env.MapboxAccessToken;
 
-export const KeplerMapContainer: FC<{
+const KeplerGl: FC<{
   mapId: string;
 }> = ({mapId}) => {
-  const root = useRef(null);
+  const bottomWidgetRef = useRef(null);
+  const theme = useTheme();
   const {keplerActions, keplerState} = useKeplerStateActions({mapId});
-
   const mapFields = keplerState
     ? mapFieldsSelector({
         id: mapId,
@@ -30,23 +35,80 @@ export const KeplerMapContainer: FC<{
       })
     : null;
 
+  const bottomWidgetFields = keplerState
+    ? bottomWidgetSelector(
+        {
+          sidePanelWidth: 300,
+          ...keplerState,
+          ...keplerActions,
+        },
+        theme,
+      )
+    : null;
+
+  return (
+    <>
+      {mapFields?.mapState ? (
+        <MapViewStateContextProvider mapState={mapFields.mapState}>
+          <MapContainer
+            primary={true}
+            containerId={0}
+            key={0}
+            index={0}
+            {...mapFields}
+            mapboxApiAccessToken={MAPBOX_TOKEN}
+          />
+        </MapViewStateContextProvider>
+      ) : null}
+      <div className="absolute bottom-0 left-0 right-0">
+        <BottomWidget
+          rootRef={bottomWidgetRef}
+          {...bottomWidgetFields}
+          // containerW={dimensions.width}
+          theme={theme}
+        />
+      </div>
+    </>
+  );
+};
+export const KeplerMapContainer: FC<{
+  mapId: string;
+}> = ({mapId}) => {
+  const root = useRef(null);
+
   return (
     <RootContext.Provider value={root}>
       <KeplerProvider mapId={mapId}>
         <div className="relative h-full w-full">
-          <div className="absolute h-full w-full kepler-gl" ref={root}>
-              {mapFields?.mapState ? (
-                <MapViewStateContextProvider mapState={mapFields.mapState}>
-                  <MapContainer
-                    primary={true}
-                    containerId={0}
-                    key={0}
-                    index={0}
-                    {...mapFields}
-                    mapboxApiAccessToken={MAPBOX_TOKEN}
-                  />
-                </MapViewStateContextProvider>
-              ) : null}
+          <div className="kepler-gl absolute h-full w-full" ref={root}>
+            <KeplerGl mapId={mapId} />
+            {/* <MapContainer
+              primary={true}
+              containerId={0}
+              key={0}
+              index={0}
+              {...mapFields}
+              mapboxApiAccessToken={MAPBOX_TOKEN}
+            {/* {mapFields?.mapState ? (
+              <MapViewStateContextProvider mapState={mapFields.mapState}>
+                <MapContainer
+                  primary={true}
+                  containerId={0}
+                  key={0}
+                  index={0}
+                  {...mapFields}
+                  mapboxApiAccessToken={MAPBOX_TOKEN}
+                />
+              </MapViewStateContextProvider>
+            ) : null}
+            <div className="absolute bottom-0 left-0 right-0">
+              <BottomWidget
+                rootRef={bottomWidgetRef}
+                {...bottomWidgetFields}
+                // containerW={dimensions.width}
+                theme={theme}
+              />
+            </div> */}
           </div>
         </div>
       </KeplerProvider>
