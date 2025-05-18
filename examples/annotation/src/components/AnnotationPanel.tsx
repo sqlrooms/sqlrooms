32379def3ca4e2e0b229cd678dcbcd59055f1fd7
Ilya Boyandin <ilyabo@gmail.com>
2025-05-18 17:21:58 +0200

    Fix warning

--- examples/annotation/src/components/AnnotationPanel.tsx
+++ examples/annotation/src/components/AnnotationPanel.tsx
@@ -1,27 +1,15 @@
 import {ProjectBuilderPanel} from '@sqlrooms/project-builder';
 import {AnnotationList} from '@sqlrooms/annotation';
 import {ProjectPanelTypes} from '../store';
-import {useProjectStore} from '../store';
 
 /**
- * TODO: This example needs to be updated to match the refactored AnnotationList component.
- *
  * The AnnotationList component no longer requires getUserName as a prop since
- * components now retrieve this directly from the store. The ts-ignore is a
- * temporary fix until the example is properly updated.
+ * components now retrieve this directly from the store.
  */
 const AnnotationPanel = () => {
-  // Keep the getUserName reference to make TypeScript happy
-  // This is a temporary fix until we update the example project
-  const getUserName = useProjectStore((s) => s.annotation.getUserName);
-
   return (
     <ProjectBuilderPanel type={ProjectPanelTypes.enum['annotations']}>
-      <AnnotationList
-        className="p-2"
-        // @ts-ignore - getUserName is no longer needed in the refactored component
-        getUserName={getUserName}
-      />
+      <AnnotationList className="p-2" />
     </ProjectBuilderPanel>
   );
 };
