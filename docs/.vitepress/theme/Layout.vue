f77d759f8af871df54b9def6a991ff0850b77919
Ilya Boyandin <ilyabo@gmail.com>
2025-05-29 21:26:59 +0200

    docs: Adding FSQ logo

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -22,6 +22,25 @@ import DefaultTheme from 'vitepress/theme';
         />
       </a>
     </template>
+
+    <template #layout-bottom>
+      <div class="foursquare-footer">
+        Supported by
+        <a
+          href="https://foursquare.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            width="100"
+            height="32"
+            src="/public/foursquare-logo.svg"
+            alt="Foursquare Logo"
+            decoding="async"
+          />
+        </a>
+      </div>
+    </template>
   </DefaultTheme.Layout>
 </template>
 
@@ -40,6 +59,36 @@ html:not(.dark) .video.dark {
 
 .dark .video.light {
   display: none;
+}
+
+.foursquare-footer {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  font-size: 0.8rem;
+  color: var(--vp-c-text-2);
+  padding: 2rem 0;
+  border-top: 1px solid var(--vp-c-divider);
+  margin-top: 2rem;
+}
+
+.foursquare-footer p {
+  margin: 0;
+  color: var(--vp-c-text-2);
+  font-size: 0.9rem;
+}
+
+.foursquare-footer a {
+  color: var(--vp-c-brand-1);
+  text-decoration: none;
+  font-weight: 500;
+}
+
+.foursquare-footer a:hover {
+  text-decoration: underline;
 }
 
 @media (min-width: 640px) {
