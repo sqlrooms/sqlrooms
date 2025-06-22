44a3ffa3d576292b3033c83c550228c045c34716
Ilya Boyandin <ilyabo@gmail.com>
2025-06-22 19:10:03 +0200

    opfs commented

--- examples/ai/src/store.ts
+++ examples/ai/src/store.ts
@@ -5,7 +5,11 @@ import {
   createDefaultAiConfig,
   getDefaultInstructions,
 } from '@sqlrooms/ai';
-import {createWasmDuckDbConnector, DataTable} from '@sqlrooms/duckdb';
+import {
+  createWasmDuckDbConnector,
+  DataTable,
+  DuckDBAccessMode,
+} from '@sqlrooms/duckdb';
 import {LayoutTypes, MAIN_VIEW} from '@sqlrooms/room-config';
 import {
   BaseRoomConfig,
@@ -27,8 +31,9 @@ import {persist} from 'zustand/middleware';
 import {DataSourcesPanel} from './components/DataSourcesPanel';
 import EchoToolResult from './components/EchoToolResult';
 import {MainView} from './components/MainView';
-import exampleSessions from './example-sessions.json';
 import {DEFAULT_MODEL} from './models';
+// import exampleSessions from './example-sessions.json';
+
 export const RoomPanelTypes = z.enum([
   'room-details',
   'data-sources',
@@ -71,7 +76,7 @@ export const {roomStore, useRoomStore} = createRoomStore<RoomConfig, RoomState>(
       // Base room slice
       ...createRoomShellSlice<RoomConfig>({
         connector: createWasmDuckDbConnector({
-          // path: 'opfs://db.duckdb',
+          // path: 'opfs://database.db',
           // accessMode: DuckDBAccessMode.READ_WRITE,
         }),
         config: {
