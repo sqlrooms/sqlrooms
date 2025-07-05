ecc1a1dd261033b1e309ad2c22ceb75cc6dfc597
Ilya Boyandin <ilyabo@gmail.com>
2025-07-05 14:51:36 +0200

    fixes

--- examples/query-motherduck/vite.config.ts
+++ examples/query-motherduck/vite.config.ts
@@ -1,32 +1,12 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
-import {VitePWA} from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'SQLRooms Query Workbench',
-        short_name: 'SQLRooms',
-        start_url: '.',
-        display: 'standalone',
-        background_color: '#ffffff',
-        description: 'Query Workbench example for SQLRooms',
-        icons: [
-          {
-            src: 'icon.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'icon.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
+  server: {
+    headers: {
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Opener-Policy': 'same-origin',
+    },
+  },
 });
