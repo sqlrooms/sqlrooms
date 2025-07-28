6eac26eb0ee1e7b58fae7f8df20286d3b5b4efe6
Ilya Boyandin <ilyabo@gmail.com>
2025-07-28 09:48:08 +0200

    fix: Create table initial query wasn't properly set

--- packages/sql-editor/src/components/CreateTableModal.tsx
+++ packages/sql-editor/src/components/CreateTableModal.tsx
@@ -49,16 +49,30 @@ export type CreateTableModalProps = {
   ) => Promise<void>;
 };
 
-const CreateTableModal: FC<CreateTableModalProps> = (props) => {
-  const {editDataSource, isOpen, onClose, onAddOrUpdateSqlQuery} = props;
+type CreateTableFormProps = {
+  query: string;
+  onClose: () => void;
+  editDataSource?: SqlQueryDataSource;
+  onAddOrUpdateSqlQuery: (
+    tableName: string,
+    query: string,
+    oldTableName?: string,
+  ) => Promise<void>;
+};
 
+const CreateTableForm: FC<CreateTableFormProps> = ({
+  query,
+  onClose,
+  editDataSource,
+  onAddOrUpdateSqlQuery,
+}) => {
   const connector = useStoreWithSqlEditor((state) => state.db.connector);
   const form = useForm<z.infer<typeof formSchema>>({
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     resolver: zodResolver(formSchema as any),
     defaultValues: {
       tableName: editDataSource?.tableName ?? '',
-      query: editDataSource?.sqlQuery ?? props.query.trim(),
+      query: editDataSource?.sqlQuery ?? query.trim(),
     },
   });
 
@@ -81,80 +95,86 @@ const CreateTableModal: FC<CreateTableModalProps> = (props) => {
   );
 
   return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <DialogHeader>
+          <DialogTitle>
+            {editDataSource ? 'Edit table query' : 'Create table from query'}
+          </DialogTitle>
+          {!editDataSource && (
+            <DialogDescription>
+              Create a new table from the results of an SQL query.
+            </DialogDescription>
+          )}
+        </DialogHeader>
+
+        {form.formState.errors.root && (
+          <Alert variant="destructive">
+            <AlertDescription className="whitespace-pre-wrap font-mono text-xs">
+              {form.formState.errors.root.message}
+            </AlertDescription>
+          </Alert>
+        )}
+
+        <FormField
+          control={form.control}
+          name="tableName"
+          render={({field}) => (
+            <FormItem>
+              <FormLabel>Table name:</FormLabel>
+              <FormControl>
+                <Input {...field} className="font-mono" autoFocus />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="query"
+          render={({field}) => (
+            <FormItem className="flex flex-col">
+              <FormLabel>SQL query:</FormLabel>
+              <FormControl>
+                <SqlMonacoEditor
+                  connector={connector}
+                  value={field.value}
+                  onChange={field.onChange}
+                  className="h-[200px] w-full"
+                  options={{
+                    scrollBeyondLastLine: false,
+                    automaticLayout: true,
+                    minimap: {enabled: false},
+                    wordWrap: 'on',
+                  }}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <DialogFooter>
+          <Button type="button" variant="outline" onClick={onClose}>
+            Cancel
+          </Button>
+          <Button type="submit" disabled={form.formState.isSubmitting}>
+            {editDataSource ? 'Update' : 'Create'}
+          </Button>
+        </DialogFooter>
+      </form>
+    </Form>
+  );
+};
+
+const CreateTableModal: FC<CreateTableModalProps> = (props) => {
+  const {isOpen, onClose} = props;
+
+  return (
     <Dialog open={isOpen} onOpenChange={(open) => !open && onClose()}>
       <DialogContent className="sm:max-w-[800px]">
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-            <DialogHeader>
-              <DialogTitle>
-                {editDataSource
-                  ? 'Edit table query'
-                  : 'Create table from query'}
-              </DialogTitle>
-              {!editDataSource && (
-                <DialogDescription>
-                  Create a new table from the results of an SQL query.
-                </DialogDescription>
-              )}
-            </DialogHeader>
-
-            {form.formState.errors.root && (
-              <Alert variant="destructive">
-                <AlertDescription className="whitespace-pre-wrap font-mono text-xs">
-                  {form.formState.errors.root.message}
-                </AlertDescription>
-              </Alert>
-            )}
-
-            <FormField
-              control={form.control}
-              name="tableName"
-              render={({field}) => (
-                <FormItem>
-                  <FormLabel>Table name:</FormLabel>
-                  <FormControl>
-                    <Input {...field} className="font-mono" autoFocus />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={form.control}
-              name="query"
-              render={({field}) => (
-                <FormItem className="flex flex-col">
-                  <FormLabel>SQL query:</FormLabel>
-                  <FormControl>
-                    <SqlMonacoEditor
-                      connector={connector}
-                      value={field.value}
-                      onChange={field.onChange}
-                      className="h-[200px] w-full"
-                      options={{
-                        scrollBeyondLastLine: false,
-                        automaticLayout: true,
-                        minimap: {enabled: false},
-                        wordWrap: 'on',
-                      }}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <DialogFooter>
-              <Button type="button" variant="outline" onClick={onClose}>
-                Cancel
-              </Button>
-              <Button type="submit" disabled={form.formState.isSubmitting}>
-                {editDataSource ? 'Update' : 'Create'}
-              </Button>
-            </DialogFooter>
-          </form>
-        </Form>
+        {isOpen && <CreateTableForm {...props} />}
       </DialogContent>
     </Dialog>
   );
