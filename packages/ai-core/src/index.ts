954f0c1978356e2f40f8fa529cb63b91b9d59723
Xun Li <lixun910@gmail.com>
2025-11-01 13:43:28 -0700

    add simple ideas argument for QueryControls

--- packages/ai-core/src/index.ts
+++ packages/ai-core/src/index.ts
@@ -11,6 +11,7 @@ export {AnalysisResult} from './components/AnalysisResult';
 export {useScrollToBottom} from './hooks/useScrollToBottom';
 export {useAiChat} from './hooks/useAiChat';
 
+export {IdeasContainer} from './components/IdeasContainer';
 export {ModelSelector} from './components/ModelSelector';
 export {SessionControls} from './components/SessionControls';
 export {QueryControls} from './components/QueryControls';
