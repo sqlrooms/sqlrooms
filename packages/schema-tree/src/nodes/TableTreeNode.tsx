7b6f470564999eb4a54452f24117169257365927
albatross97 <albatross7497@gmail.com>
2025-11-05 09:28:26 -0800

    more changes

--- packages/schema-tree/src/nodes/TableTreeNode.tsx
+++ packages/schema-tree/src/nodes/TableTreeNode.tsx
@@ -102,14 +102,16 @@ export const TableTreeNode: FC<{
   return (
     <>
       <BaseTreeNode asChild className={className} nodeObject={nodeObject}>
-        <div className="relative flex w-full items-center space-x-2">
+        <div className="relative flex w-full items-center space-x-2 pr-6">
           {isView ? (
             <ViewIcon size="16px" className="shrink-0 text-blue-500" />
           ) : (
             <TableIcon size="16px" className="shrink-0 text-blue-500" />
           )}
           <div className="flex w-full items-center justify-between gap-2">
-            <span className="truncate">{name}</span>
+            <span className="truncate" title={name}>
+              {name}
+            </span>
             {rowCount !== undefined && (
               <span className="text-muted-foreground/50 ml-1 whitespace-nowrap pr-5 text-xs">
                 {formatCount(rowCount)} {rowCount === 1 ? 'row' : 'rows'}
