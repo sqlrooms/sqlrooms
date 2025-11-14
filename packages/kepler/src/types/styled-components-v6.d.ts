a0ee6965cdbc54e113ef5b5aaecc7fb6288e1344
Xun Li <lixun910@gmail.com>
2025-11-13 17:27:29 -0700

    revert changes in package.json; remove kepler related d.ts; revert textarea.tsx

--- packages/kepler/src/types/styled-components-v6.d.ts
+++
@@ -1,9 +0,0 @@
-import 'styled-components';
-
-declare module 'styled-components' {
-  export interface StyleSheetManagerProps {
-    shouldForwardProp?: (propName: string, elementToBeRendered: any) => boolean;
-  }
-}
-
-
--- packages/ui/src/components/textarea.tsx
+++ packages/ui/src/components/textarea.tsx
@@ -38,6 +38,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
       <textarea
         className={cn(
           'border-input placeholder:text-muted-foreground focus-visible:ring-ring flex min-h-[60px] w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-sm focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+          autoResize ? 'overflow-y-hidden' : undefined,
           className,
         )}
         ref={localRef}
