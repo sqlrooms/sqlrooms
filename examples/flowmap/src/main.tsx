8919d3e910812d7ad868725db648f3c8fee0adc0
Ilya Boyandin <ilyabo@gmail.com>
2025-12-28 20:43:08 +0100

    bundle monaco
    Signed-off-by: Ilya Boyandin <ilyabo@gmail.com>

--- examples/flowmap/src/main.tsx
+++ examples/flowmap/src/main.tsx
@@ -4,6 +4,13 @@ import {createRoot} from 'react-dom/client';
 import {Room} from './room';
 import './index.css';
 
+// Configure the monaco loader to bundle the editor with Vite for offline work
+import {configureMonacoLoader} from '@sqlrooms/monaco-editor';
+import * as monaco from 'monaco-editor';
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+
+configureMonacoLoader({monaco, workers: {default: editorWorker}});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="light" storageKey="sqlrooms-ui-theme">
