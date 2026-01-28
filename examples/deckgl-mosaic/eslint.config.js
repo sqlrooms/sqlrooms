24b8619f33b784bbe5b853b465cbde350209b8e0
Dmitriy Kostianetskiy <dkostianetskiy@foursquare.com>
2026-01-28 13:41:18 +0100

    fix: eslint configuration (#317)
    * fix: eslint errors
    
    Parsing error: No tsconfigRootDir was set, and multiple candidate TSConfigRootDirs are present.
    
    * chore: add js files to lintstage

--- examples/deckgl-mosaic/eslint.config.js
+++ examples/deckgl-mosaic/eslint.config.js
@@ -3,15 +3,19 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import {defineConfig} from 'eslint/config';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default defineConfig(
+  {ignores: ['dist']},
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     plugins: {
       'react-hooks': reactHooks,
@@ -21,7 +25,7 @@ export default tseslint.config(
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
-        { allowConstantExport: true },
+        {allowConstantExport: true},
       ],
     },
   },
