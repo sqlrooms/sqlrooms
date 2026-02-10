085b29edbe4af65e320280d1649d6c07637e40e2
Ilya Boyandin <ilyabo@gmail.com>
2026-02-10 15:20:07 +0100

    styling
    Signed-off-by: Ilya Boyandin <ilyabo@gmail.com>

--- packages/schema-tree/src/nodes/TableTreeNode.tsx
+++ packages/schema-tree/src/nodes/TableTreeNode.tsx
@@ -2,7 +2,7 @@
 
 import {DataTableModal} from '@sqlrooms/data-table';
 import {makeQualifiedTableName, TableNodeObject} from '@sqlrooms/duckdb';
-import {useDisclosure} from '@sqlrooms/ui';
+import {cn, useDisclosure} from '@sqlrooms/ui';
 import {formatCount} from '@sqlrooms/utils';
 import {CopyIcon, EyeIcon, TableIcon, ViewIcon} from 'lucide-react';
 import {FC} from 'react';
@@ -109,7 +109,13 @@ export const TableTreeNode: FC<{
             <TableIcon size="16px" className="shrink-0 text-blue-500" />
           )}
           <div className="flex min-w-0 flex-1 items-center gap-2 overflow-hidden">
-            <span className="max-w-[70%] shrink-0 truncate" title={name}>
+            <span
+              className={cn(
+                'shrink-0 truncate',
+                rowCount !== undefined && rowCount > 0 && 'max-w-[70%]',
+              )}
+              title={name}
+            >
               {name}
             </span>
             {rowCount !== undefined && rowCount > 0 && (
