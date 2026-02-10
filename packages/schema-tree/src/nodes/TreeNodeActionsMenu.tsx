cfa308dd8edb9138fc73fcec5bd0f8e12658966d
Ilya Boyandin <ilyabo@gmail.com>
2026-02-10 15:26:50 +0100

    styling
    Signed-off-by: Ilya Boyandin <ilyabo@gmail.com>

--- packages/schema-tree/src/nodes/TreeNodeActionsMenu.tsx
+++ packages/schema-tree/src/nodes/TreeNodeActionsMenu.tsx
@@ -36,7 +36,7 @@ export const TreeNodeActionsMenu: FC<TreeNodeActionsMenuProps> = (props) => {
     <div className="absolute right-0 top-[1px] h-full opacity-0 outline-none group-hover:opacity-100">
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <div className="bg-muted fg-muted-foreground hover:fg-foreground flex h-full w-7 items-center justify-center p-0 outline-none transition-colors">
+          <div className="bg-muted text-muted-foreground hover:text-foreground flex h-full w-7 items-center justify-center p-0 outline-none transition-colors">
             <EllipsisVerticalIcon size="15px" className="relative top-[-1px]" />
           </div>
         </DropdownMenuTrigger>
