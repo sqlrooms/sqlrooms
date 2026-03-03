725a59fafd920e4923be282a92fae6e51e4660d8
Jonathan <jandrade@foursquare.com>
2026-03-03 14:25:47 -0500

    chore: add richColors prop to Sonner

--- packages/ui/src/components/toaster.tsx
+++ packages/ui/src/components/toaster.tsx
@@ -46,10 +46,11 @@ export function Toaster(props: ToasterProps) {
       <Sonner
         theme={theme as ToasterProps['theme']}
         className="toaster group"
+        richColors
         toastOptions={{
           classNames: {
             toast:
-              'group toast group-[.toaster]:border-border group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:shadow-lg',
+              'group toast group-[.toaster]:text-foreground group-[.toaster]:shadow-lg',
             description: 'group-[.toast]:text-muted-foreground',
             actionButton:
               'group-[.toast]:bg-primary group-[.toast]:text-primary-foreground',
