0e05919b5c6860f0f24f03cda7a15de4a58e110e
Jonathan <jandrade@foursquare.com>
2026-03-06 15:45:19 -0500

    fix: add sonner overrides on preset

--- packages/ui/src/components/toaster.tsx
+++ packages/ui/src/components/toaster.tsx
@@ -64,6 +64,23 @@ export const Toaster = ({...props}: ToasterProps) => {
           '--normal-bg': 'var(--color-popover)',
           '--normal-text': 'var(--color-popover-foreground)',
           '--normal-border': 'var(--color-border)',
+
+          '--success-bg': 'var(--color-popover)',
+          '--success-text': 'var(--color-popover-foreground)',
+          '--success-border': 'var(--color-border)',
+
+          '--info-bg': 'var(--color-popover)',
+          '--info-text': 'var(--color-popover-foreground)',
+          '--info-border': 'var(--color-border)',
+
+          '--warning-bg': 'var(--color-popover)',
+          '--warning-text': 'var(--color-popover-foreground)',
+          '--warning-border': 'var(--color-border)',
+
+          '--error-bg': 'var(--color-destructive)',
+          '--error-text': 'var(--color-destructive-foreground)',
+          '--error-border': 'var(--color-destructive)',
+
           '--border-radius': 'var(--radius)',
         } as React.CSSProperties
       }
