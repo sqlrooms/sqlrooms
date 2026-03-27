9efc75cf2760b7a01e5316b598b82e457353d384
Ilya Boyandin <ilyabo@gmail.com>
2026-03-27 08:14:57 +0100

    chore(ui): upgrade react-resizable-panels to v4

--- packages/ui/src/components/resizable.tsx
+++ packages/ui/src/components/resizable.tsx
@@ -5,31 +5,66 @@ import * as ResizablePrimitive from 'react-resizable-panels';
 
 import {cn} from '../lib/utils';
 
+type LegacyDirection = 'horizontal' | 'vertical';
+
+type ResizablePanelGroupProps = Omit<
+  React.ComponentProps<typeof ResizablePrimitive.Group>,
+  'orientation'
+> & {
+  direction?: LegacyDirection;
+  orientation?: LegacyDirection;
+};
+
 const ResizablePanelGroup = ({
+  direction,
+  orientation,
   className,
   ...props
-}: React.ComponentProps<typeof ResizablePrimitive.PanelGroup>) => (
-  <ResizablePrimitive.PanelGroup
+}: ResizablePanelGroupProps) => (
+  <ResizablePrimitive.Group
+    orientation={orientation ?? direction}
     className={cn(
-      'flex h-full w-full data-[panel-group-direction=vertical]:flex-col',
+      'flex h-full w-full aria-orientation-vertical:flex-col',
       className,
     )}
     {...props}
   />
 );
 
-const ResizablePanel = ResizablePrimitive.Panel;
+type ResizableSize = string | number | undefined;
+type ResizablePanelProps = React.ComponentProps<
+  typeof ResizablePrimitive.Panel
+>;
+
+const asLegacyPercent = (size: ResizableSize): ResizableSize =>
+  typeof size === 'number' ? `${size}%` : size;
+
+const ResizablePanel = ({
+  defaultSize,
+  minSize,
+  maxSize,
+  collapsedSize,
+  ...props
+}: ResizablePanelProps) => (
+  <ResizablePrimitive.Panel
+    defaultSize={asLegacyPercent(defaultSize)}
+    minSize={asLegacyPercent(minSize)}
+    maxSize={asLegacyPercent(maxSize)}
+    collapsedSize={asLegacyPercent(collapsedSize)}
+    {...props}
+  />
+);
 
 const ResizableHandle = ({
   withHandle,
   className,
   ...props
-}: React.ComponentProps<typeof ResizablePrimitive.PanelResizeHandle> & {
+}: React.ComponentProps<typeof ResizablePrimitive.Separator> & {
   withHandle?: boolean;
 }) => (
-  <ResizablePrimitive.PanelResizeHandle
+  <ResizablePrimitive.Separator
     className={cn(
-      'bg-border focus-visible:ring-ring relative flex w-px items-center justify-center after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:outline-hidden data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:translate-x-0 data-[panel-group-direction=vertical]:after:-translate-y-1/2 [&[data-panel-group-direction=vertical]>div]:rotate-90',
+      'bg-border focus-visible:ring-ring relative flex w-px items-center justify-center after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:outline-hidden aria-orientation-vertical:h-px aria-orientation-vertical:w-full aria-orientation-vertical:after:left-0 aria-orientation-vertical:after:h-1 aria-orientation-vertical:after:w-full aria-orientation-vertical:after:translate-x-0 aria-orientation-vertical:after:-translate-y-1/2 [&[aria-orientation=vertical]>div]:rotate-90',
       className,
     )}
     {...props}
@@ -39,7 +74,7 @@ const ResizableHandle = ({
         <GripVertical className="h-2.5 w-2.5" />
       </div>
     )}
-  </ResizablePrimitive.PanelResizeHandle>
+  </ResizablePrimitive.Separator>
 );
 
 export {ResizablePanelGroup, ResizablePanel, ResizableHandle};
