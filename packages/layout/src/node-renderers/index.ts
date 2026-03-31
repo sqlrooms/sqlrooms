7704ddf2b1b15ee7aea891eb64879fb39d949fdc
Ilya Boyandin <ilyabo@gmail.com>
2026-03-31 21:00:39 +0200

    knip
    Signed-off-by: Ilya Boyandin <ilyabo@gmail.com>

--- packages/layout/src/node-renderers/index.ts
+++
@@ -1,7 +0,0 @@
-export {LeafRenderer} from './LeafRenderer';
-export {MosaicRenderer} from './MosaicRenderer';
-export {NodeRenderer} from './NodeRenderer';
-export {SplitRenderer} from './SplitRenderer';
-export {TabsRenderer} from './TabsRenderer';
-export type {NodeRenderProps} from './types';
-export {lookupPanelInfo} from './types';
