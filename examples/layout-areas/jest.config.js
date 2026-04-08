1f9400ab2d554896b3635a9bc664a79e52c53f3a
Dmitriy Kostianetskiy <dkostianetskiy@foursquare.com>
2026-04-08 16:43:09 +0200

    fix: jest configuration

--- examples/layout-areas/jest.config.js
+++
@@ -1,7 +0,0 @@
-import base from '@sqlrooms/preset-jest/base.js';
-
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-export default {
-  ...base,
-  displayName: 'layout-areas-example',
-};
--- packages/layout/jest.config.js
+++ packages/layout/jest.config.js
@@ -1,7 +1,6 @@
-import reactConfig from '@sqlrooms/preset-jest/react.js';
+import nodeConfig from '@sqlrooms/preset-jest/node.js';
 
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-  ...reactConfig,
-  displayName: 'layout',
+  ...nodeConfig,
 };
