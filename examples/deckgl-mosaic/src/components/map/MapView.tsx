09fe3c73ac29722113266e92a37cdad79aa7fa81
Ilya Boyandin <ilyabo@gmail.com>
2026-05-05 11:56:06 +0200

    prevent flicker
    Signed-off-by: Ilya Boyandin <ilyabo@gmail.com>

--- examples/deckgl-mosaic/src/components/map/MapView.tsx
+++ examples/deckgl-mosaic/src/components/map/MapView.tsx
@@ -55,11 +55,7 @@ export const MapView: FC<{className?: string}> = ({className}) => {
 
   const lastUpdateRef = useRef<number>(0);
 
-  const {
-    data: rawData,
-    isLoading,
-    client,
-  } = useMosaicClient({
+  const {data: rawData, client} = useMosaicClient({
     selectionName: 'brush',
     query: (filter: any) =>
       Query.from('earthquakes')
@@ -81,7 +77,7 @@ export const MapView: FC<{className?: string}> = ({className}) => {
     }),
     [rawData],
   );
-  const dbReady = !isLoading && rawData !== null;
+  const dbReady = rawData !== null;
   const {resolvedTheme} = useTheme();
   const colorScale = useMemo(() => {
     return {
