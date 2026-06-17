8b3f039ab356f69aec1379d487d22f8c3bfe7113
Ilya Boyandin <ilyabo@gmail.com>
2026-06-17 12:38:09 +0200

    removed unused

--- packages/ai-core/src/components/AnalysisAnswer.tsx
+++
@@ -1,8 +0,0 @@
-/** @deprecated Use `MessageContent` from `./MessageContent` instead. */
-export {MessageContent as AnalysisAnswer} from './MessageContent';
-
-/** @deprecated Use `processMessageContent` from `./MessageContent` instead. */
-export {processMessageContent as processAnalysisAnswerContent} from './MessageContent';
-
-/** @deprecated Use `MessageContentProps` from `./MessageContent` instead. */
-export type {MessageContentProps as AnalysisAnswerProps} from './MessageContent';
--- packages/ai-core/src/components/AnalysisResult.tsx
+++
@@ -1,5 +0,0 @@
-/** @deprecated Use `ChatTurnView` from `./ChatTurnView` instead. */
-export {ChatTurnView as AnalysisResult} from './ChatTurnView';
-
-/** @deprecated Use `ChatTurnViewProps` from `./ChatTurnView` instead. */
-export type {ChatTurnViewProps as AnalysisResultProps} from './ChatTurnView';
