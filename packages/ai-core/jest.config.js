ed88ff7e352d74f7350c4fb45bc6f1f0dcb3da14
Ilya Boyandin <ilyabo@gmail.com>
2026-08-06 17:18:08 +0200

    fix: address Dependabot vulnerabilities

--- packages/ai-core/jest.config.js
+++ packages/ai-core/jest.config.js
@@ -3,4 +3,8 @@ import nodeConfig from '@sqlrooms/preset-jest/node.js';
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
   ...nodeConfig,
+  setupFilesAfterEnv: [
+    ...(nodeConfig.setupFilesAfterEnv ?? []),
+    '<rootDir>/test/setup.ts',
+  ],
 };
