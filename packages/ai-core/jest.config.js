ec0ff7e1c33f9f6798c73cbea95d9f3a58d540f9
Ilya Boyandin <ilyabo@gmail.com>
2026-08-06 17:28:41 +0200

    fix: use same-major AI SDK security updates

--- packages/ai-core/jest.config.js
+++ packages/ai-core/jest.config.js
@@ -3,8 +3,4 @@ import nodeConfig from '@sqlrooms/preset-jest/node.js';
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
   ...nodeConfig,
-  setupFilesAfterEnv: [
-    ...(nodeConfig.setupFilesAfterEnv ?? []),
-    '<rootDir>/test/setup.ts',
-  ],
 };
